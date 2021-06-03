c56d70846b4de6d496d655c6b0e718a22eb13a18
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-03 17:09:38 -0300

    feat(client): base structuring of the reading file process
    - implementation of the try-with-resources for reading the file
    - declaration of the path to the video
    - declaration of the buffer size to be up to 8kb

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,19 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Client {
 
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "client/c1/video-test.mp4";
+        int bufferSize = 8 * 1024;
+        try ( InputStream reader = new BufferedInputStream(new FileInputStream(path), bufferSize) ) {
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }        
     }
 }
