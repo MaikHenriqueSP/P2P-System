8b01a1d76382174099cb2fd72012b30c877bb5c5
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-03 17:20:22 -0300

    feat(client): creation of the reading file cycle
    - creation of .gitignore file to ignore test the content

--- src/Client.java
+++ src/Client.java
@@ -7,11 +7,15 @@ public class Client {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        String path = "client/c1/video-test.mp4";
+        String path = "src/client/c1/video-test.mp4";
         int bufferSize = 8 * 1024;
         try ( InputStream reader = new BufferedInputStream(new FileInputStream(path), bufferSize) ) {
+            byte[] data = new byte[bufferSize];
             
+            while (reader.read(data, 0, data.length) != -1) {
 
+
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }        
