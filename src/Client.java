4c5adca54c956e66034c9c72665565013a06a727
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-04 09:53:51 -0300

    feat(client): creation of method and class base for file transfering
    - FileServerThread is intended to enable transfering on a separate thread
    - runFilesShareServer is the method that allows the client to behave as a sharing files server

--- src/Client.java
+++ src/Client.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Client {
     private ServerSocket server;
@@ -24,6 +25,27 @@ public class Client {
         String clientFolderPath = BASE_CLIENT_FOLDER_PATH + clientName;
         File dirs = new File(clientFolderPath);
         dirs.mkdirs();
+    }
+
+    class FileServerThread extends Thread {
+        private Socket client;
+
+        public FileServerThread(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
+        
+    }
+
+    public void runFilesShareServer() throws IOException {
+        while (true) {
+            Socket client = server.accept();    
+            new FileServerThread(client).start();
+        }
     }
 
     public static void main(String[] args) throws IOException {
