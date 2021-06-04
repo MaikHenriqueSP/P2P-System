956fead358101e98232aee9ad7a6c6ecabe25f0e
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-04 10:16:55 -0300

    feat(client): addition of user input for port and client number

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,12 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,17 +14,17 @@ import java.net.Socket;
 public class Client {
     private ServerSocket server;
     private String clientName;
-    private static final String BASE_CLIENT_FOLDER_PATH = "/src/client/resource/";
+    private static final String BASE_CLIENT_FOLDER_PATH = "src/client/resource/";
     
 
-    public Client(ServerSocket server, int port, String clientName) throws IOException {
+    public Client(int port, String clientName) throws IOException {
         this.server = new ServerSocket(port);
         this.clientName = clientName;
-        createClientFolder();
+        createClientFolderIfNotExists();
     }
 
-    private void createClientFolder() {
-        String clientFolderPath = BASE_CLIENT_FOLDER_PATH + clientName;
+    private void createClientFolderIfNotExists() {
+        String clientFolderPath = BASE_CLIENT_FOLDER_PATH + clientName.toLowerCase();
         File dirs = new File(clientFolderPath);
         dirs.mkdirs();
     }
@@ -49,6 +51,13 @@ public class Client {
     }
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Type the client's sharing port number:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int port = Integer.parseInt(reader.readLine());
+        System.out.println("Type the client's name:");
+        String clientName = reader.readLine();
 
+        Client client = new Client(port, clientName);
+        client.runFilesShareServer();
     }
 }
