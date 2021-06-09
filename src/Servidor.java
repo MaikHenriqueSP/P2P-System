5ac9da803124dfbceae192671644546dc08e6463
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-09 17:39:17 -0300

    feat(servidor): implementation of the iniciaServidor method
    - this method server the purpose of initializating the server to receive packets from clients via UDP

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,12 +1,56 @@
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
 
 public class Servidor {
-    
-    private final ServerSocket socketReceptor;
+
+    private final DatagramSocket socketReceptor;
     private static final int PORTA_SOCKET_RECEPTOR = 10098;
     
-    public Servidor(ServerSocket socketReceptor) throws IOException {
-        this.socketReceptor = new ServerSocket(PORTA_SOCKET_RECEPTOR);
+    public Servidor() throws IOException {
+        this.socketReceptor = new DatagramSocket(PORTA_SOCKET_RECEPTOR);
+    }
+
+    public void iniciarServidor() throws IOException, ClassNotFoundException {
+        while (true) {
+            byte[] receivedBytes = new byte[8 * 1024];
+            DatagramPacket packet = new DatagramPacket(receivedBytes, receivedBytes.length);
+            socketReceptor.receive(packet);
+
+
+
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(packet.getData());
+            ObjectInputStream inputObject = new ObjectInputStream(new BufferedInputStream(byteArrayInputStream));
+            inputObject.close();
+         
+            Mensagem mensagem = (Mensagem) inputObject.readObject();
+            String requisicao = mensagem.getTitulo();
+
+            switch (requisicao) {
+                case "a":
+                    System.out.println("a");
+                
+            }
+            
+            
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Servidor servidor = new Servidor();
+            servidor.iniciarServidor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
