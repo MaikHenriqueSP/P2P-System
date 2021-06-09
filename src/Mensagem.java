d435afe1581f124d110031474e387b333fe3ec61
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-09 08:32:27 -0300

    feat(mensagem): creation of the instance variables
    - creation of the method adicionarMensagem
    - creation of getters
    - creation of the titulo and mensagens instance variables

--- src/Mensagem.java
+++ src/Mensagem.java
@@ -1,7 +1,29 @@
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Mensagem implements Serializable {
 
     private static final long serialVersionUID = -3969352858203924755L;
+
+    private String titulo;
+    private Map<String, Object> mensagens;
+    
+    public Mensagem(String titulo) {
+        this.titulo = titulo;
+        this.mensagens = new HashMap<>();
+    }
+
+    public void adicionarMensagem(String titulo, Object corpoMensagem) {
+        mensagens.put(titulo, corpoMensagem);
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Map<String, Object> getMensagens() {
+        return mensagens;
+    }
 
 }
