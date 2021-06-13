cec282d5e6dd3099ad458323ab2e09d144c63dc3
Maik Henrique <maik.hsp.contato@gmail.com>
2021-06-13 20:06:51 -0300

    doc(mensagem): writing the basic Mensagem documentation

--- src/Mensagem.java
+++ src/Mensagem.java
@@ -14,18 +14,30 @@ import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Responsável pelo template de mensagens, sendo composto pelo título e um conjunto de mapeamentos chave-valor 
+ * como corpo da mensagem em si.
+ * 
+ * @author Maik Henrique
+ */
 public class Mensagem implements Serializable {
 
     private static final long serialVersionUID = -3969352858203924755L;
 
     private final String titulo;
-    private final Map<String, Object> mensagens;
-    
+    private final Map<String, Object> mensagens;    
+
     public Mensagem(String titulo) {
         this.titulo = titulo;
         this.mensagens = new HashMap<>();
     }
 
+    /**
+     * Responsavél pela inserção de um mapeamento chave-valor ao corpo da mensagem.
+     * 
+     * @param titulo título da mensagem.
+     * @param corpoMensagem item do corpo da mensagem.
+     */
     public void adicionarMensagem(String titulo, Object corpoMensagem) {
         mensagens.put(titulo, corpoMensagem);
     }
@@ -38,7 +50,12 @@ public class Mensagem implements Serializable {
         return mensagens;
     }
 
-
+    /**
+     * Utilitário para recebimento de mensagens TCP.
+     * 
+     * @param inputStream stream de input da conexão TCP entre as partes.
+     * @return mensagem recebida
+     */
     public static Mensagem receberMensagemTCP(InputStream inputStream) {
         try {
             ObjectInputStream objectInputStream = new ObjectInputStream(new BufferedInputStream(inputStream));
@@ -49,6 +66,12 @@ public class Mensagem implements Serializable {
         return null;
     }
 
+    /**
+     * Utilitário para envios de de mensagens TCP.
+     * 
+     * @param outputStream stream escrita da conexão TCP
+     * @param mensagem mensagem a ser enviada para o outro extremo da conexão
+     */
     public static void enviarMensagemTCP(OutputStream outputStream, Mensagem mensagem) {
         try {
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(new BufferedOutputStream(outputStream));
@@ -59,6 +82,14 @@ public class Mensagem implements Serializable {
         }
     }
 
+    /**
+     * Utilitário para o envio de mensagens UDP.
+     * 
+     * @param mensagem mensagem a ser enviada ao destinatário.
+     * @param endereco endereço IP do destinatário @TODO arrumar a questão do localhost usando InetAddress.
+     * @param porta porta do destinatário.
+     * @param socketUDP socket UDP utilitário para o envio.
+     */
     public static void enviarMensagemUDP(Mensagem mensagem, String endereco, int porta, DatagramSocket socketUDP) {
         InetAddress enderecoDestinatarioInet;
         try {
@@ -77,6 +108,12 @@ public class Mensagem implements Serializable {
         }        
     }
 
+    /**
+     * Utilitário para recebimento de mensagens UDP.
+     * 
+     * @param socketUDP socket UDP utilitário para o recebimento de mensagem.
+     * @return devolve uma instância construída da mensagem recebida.
+     */
     public static Mensagem receberMensagemUDP(DatagramSocket socketUDP) {
         byte[] bytesRecebidos = new byte[8 * 1024];
         DatagramPacket pacote = new DatagramPacket(bytesRecebidos, bytesRecebidos.length);
